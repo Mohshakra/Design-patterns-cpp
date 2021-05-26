040c70d274209274c63ad04013a0b7b395076c90
Mohamed Shakra <mohamed.shakra@scania.com>
2021-05-26 16:50:11 +0200

    Strategy pattern

--- strategy/stratgy.cpp
+++ strategy/stratgy.cpp
@@ -2,41 +2,47 @@
 #include <string>
 #include <memory>
 
-struct Istrategy{ 
-	virtual ~Istrategy(){}
+struct IStrategy{ 
+	virtual ~IStrategy(){std::cout << std::endl;}
 	
 	virtual auto compute(double x, double y) const -> double = 0;
 	virtual auto name() const -> const std::string = 0; 
-	virtual auto clone() const -> Istrategy* = 0;
-}
+	virtual auto printa() const -> const std::string = 0;
+
+	virtual auto clone() const -> IStrategy* = 0;
+};
 
 
 class Context{
 	
 private :
 	
-	std::unique_ptr<Istrategy> _strategy;
+	std::unique_ptr<IStrategy> _strategy;
 	
 public :
 
 	Context()
 		: _strategy(nullptr) {}
-	Context(Istrategy* s)
+	Context(IStrategy* s)
 		: _strategy{s} {}
-	Context
-		: _strategy{s.clone} {}
+	Context(IStrategy& s)
+		: _strategy{s.clone()} {
+			std::cout << "hello world";
+		}
 	auto setStrategy(IStrategy* s){
 		_strategy.reset(s);
 		}
-	auto setStrategy(const IStrategy& s){
-		_strategy.reset(s.clone());
+
+
+
+	// auto setStrategy(const IStrategy& s){
+	// 	_strategy.reset(s.clone());
 		
-		}
+	// 	}
 		
 	auto compute(double x, double y) -> void {
 		if(_strategy == nullptr)
 			std::runtime_error("ERROR: strategy not set");
-			
 		double result = _strategy->compute(x, y);
 		std::cout << " Strategy = " << _strategy->name() << " "
 				
@@ -44,15 +50,94 @@ public :
 				<< "y = " << y << " )"
 				<< "\n";
 				std::cout << "Result = " << result << "\n";
-				
-				
-				
-				
-				
-		
-		
 		
 		}
 
 };
 
+
+class AddStrategy: public IStrategy 
+{
+
+	public :
+		auto name() const -> const std::string{
+			return "add";
+		}
+		auto compute(double x, double y) const -> double {
+			return x + y ; 
+		}
+		auto clone() const -> IStrategy* {
+			std::cerr << "\n     [Trace] AddStrategy => I was cloned "<< "\n" ;
+			return new AddStrategy(*this);
+		}
+		auto printa() const -> const std::string{
+			// std::cout << "====== Straregy = Sub =====" << std::endl;
+			return "hello";
+		}
+
+};
+
+class MulStrategy: public IStrategy 
+{
+	public :
+
+		auto name() const -> const std::string{
+			return "Multiplication";
+		}
+		auto compute(double x, double y) const -> double {
+			return x * y;
+		}
+		auto clone() const -> IStrategy*{
+			std::cerr << "\n  [Trace] MulStrategy => I was cloned" << "\n";
+			return new MulStrategy(*this);
+		}
+		auto printa() const -> const std::string{
+			// std::cout << "====== Straregy = Sub =====" << std::endl;
+			return "hello";
+		}
+
+
+};
+
+class SubStrategy: public IStrategy 
+{
+
+	public :
+		auto name() const -> const std::string{
+			return "Subtract";
+		}
+		auto compute(double x, double y) const -> double {
+			return x - y ; 
+		}
+		auto clone() const -> IStrategy* {
+			std::cerr << "\n    [Trace] AddStrategy => I was cloned "<< "\n" ;
+			return new SubStrategy(*this);
+		}
+		auto printa() const -> const std::string{
+			// std::cout << "====== Straregy = Sub =====" << std::endl;
+			return "hello";
+		}
+	
+
+};
+
+
+int main(){
+
+	Context ctx;
+	// std::cout << "====== Straregy = add =====" << "\n";
+	ctx.setStrategy(new AddStrategy);
+	ctx.compute(3.0, 4.0);
+
+	// std::cout <<  "====== Straregy = mul =====" << "\n";
+	ctx.setStrategy(new MulStrategy);
+	ctx.compute(2, 5);
+
+	// std::cout <<  "====== Straregy = Sub =====" << "\n";
+	ctx.setStrategy(new SubStrategy);
+	ctx.compute(5, 1);
+
+
+
+	return 0;
+}
